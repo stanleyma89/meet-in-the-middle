3f0f01618c3e77bf76d1d7bd73e36a5dfad025d5
Gina Son <ginasunah33@gmail.com>
2017-08-31 15:23:27 -0400

    add submit button to set up the locations

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,18 +1,6 @@
-var map;
-
 // Initializes map
-function initMap() {
 
- var bitmaker = {lat: 43.647649, lng: -79.387131};
- // var operaHouse = {lat: 43.659213, lng: -79.348781};
- var brampton = {lat: 43.698739, lng: -79.781257};
- var rich = {lat: 43.887714, lng: -79.438603};
- var markers = [bitmaker, brampton, rich];
- var locations = [
-         [43.647649, -79.387131],
-        //  [43.659213, -79.348781],
-         [43.698739, -79.781257],
-         [43.887714, -79.438603]];
+function initMap() {
 
 // Creates map and sets starting view and zoom
 var map = new google.maps.Map(document.getElementById('map'), {
@@ -20,14 +8,65 @@ var map = new google.maps.Map(document.getElementById('map'), {
    zoom: 11
  });
 
-//  Places markers on all location in 'markers'
-// for (var i = 0; i < markers.length; i++) {
-//   var marker = new google.maps.Marker({
-//      position: markers[i],
-//      map: map
-//    });
-//   };
+var input = document.getElementById('pac-input');
+var output = document.getElementById('pac-output');
+var submit = document.getElementById('submit');
 
+// var autocompleteA = new google.maps.places.Autocomplete(input);
+var autocompleteB = new google.maps.places.Autocomplete(output);
+
+// autocompleteA.bindTo('bounds', map);
+autocompleteB.bindTo('bounds', map);
+
+// autocompleteA.addListener('place_changed', function(){
+//
+//   var placeA = autocompleteA.getPlace();
+//   // console.log(placeA);
+//   if (placeA.geometry.viewport) {
+//     map.fitBounds(placeA.geometry.viewport);
+//   } else {
+//     map.setCenter(placeA.geometry.viewport);
+//     map.setZoom(17);
+//   }
+// });
+
+
+
+var locations = [];
+
+autocompleteB.addListener('place_changed', function(){
+var lonLat = [];
+
+  var placeB = autocompleteB.getPlace();
+
+  lonLat.push(placeB.geometry.viewport.f.b);
+  lonLat.push(placeB.geometry.viewport.b.b);
+
+  locations.push(lonLat);
+
+   console.log(locations);
+  //  initMap(locations)
+  // reload(locations);
+
+  if (placeB.geometry.viewport) {
+    map.fitBounds(placeB.geometry.viewport);
+  } else {
+    map.setCenter(placeB.geometry.viewport);
+    map.setZoom(17);
+  }
+
+
+  submit.addEventListener('click', function(){
+    reload(locations);
+  })
+
+});
+
+
+
+
+function reload(locations){
+  console.log(locations);
 //  Creates rectangular boundary using all cooridates in 'locations'
   var bound = new google.maps.LatLngBounds();
    for (i = 0; i < locations.length; i++) {
@@ -36,24 +75,37 @@ var map = new google.maps.Map(document.getElementById('map'), {
 
 // Finds center point of recanglular boundary created above and places marker. Stores center location in 'center'
   var center = { lng: parseFloat(bound.getCenter().lng()), lat: parseFloat(bound.getCenter().lat())};
-  var marker = new google.maps.Marker({
+  // console.log(center);
+  var centerMarker = new google.maps.Marker({
     position: center,
     map: map
   });
+  initialize(center, centerMarker);
 
-// Initializes directions services
-  var directionsDisplay;
-  var directionsService = new google.maps.DirectionsService();
-  var map;
+}
+
+//moved inside initialize function()
+// var directionsDisplay;
+// var directionsService = new google.maps.DirectionsService();
+
 
 // Changes display parameters of map (may not need one at start?)
-  function initialize() {
+  function initialize(center, centerMarker) {
+
+
+
+    // Initializes directions services
+      var directionsDisplay;
+      var directionsService = new google.maps.DirectionsService();
+
+
+
     directionsDisplay = new google.maps.DirectionsRenderer();
     var awal = new google.maps.LatLng(center);
     var mapOptions = {
       zoom: 11,
       center: awal
-    }
+    };
     // map = new google.maps.Map(document.getElementById('map'), mapOptions);
     // directionsDisplay.setMap(map);
     var i;
@@ -62,7 +114,7 @@ var map = new google.maps.Map(document.getElementById('map'), {
       for (i = 0; i < locations.length; i++) {
         directionsServices[i] = new google.maps.DirectionsService();
         var start = new google.maps.LatLng(locations[i][0], locations[i][1]);
-        var end = new google.maps.LatLng(locations[2][0], locations[2][1]);
+
 
         var request = {
             origin: start,
@@ -94,28 +146,30 @@ var map = new google.maps.Map(document.getElementById('map'), {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < results.length; i++) {
         createMarker(results[i]);
+        // console.log(results[i]);
       }
     }
   }
 
   function createMarker(place) {
     var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
+    var markers = new google.maps.Marker({
       map: map,
+      icon: place.icon,
       position: place.geometry.location
     });
 
-    google.maps.event.addListener(marker, 'click', function() {
+    google.maps.event.addListener(markers, 'click', function() {
       infowindow.setContent(place.name);
       infowindow.open(map, this);
     });
   }
 
-  var circle = new google.maps.Circle({
-    map: map,
-    radius: 1000,    // 10 miles in metres
-    fillColor: '#AA0000 '
-  });
-  circle.bindTo('center', marker, 'position');
+  // var circle = new google.maps.Circle({
+  //   map: map,
+  //   radius: 1000,    // 10 miles in metres
+  //   fillColor: '#AA0000 '
+  // });
+  // circle.bindTo('center', markers, 'position');
 
 }
