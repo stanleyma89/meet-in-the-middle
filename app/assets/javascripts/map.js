642a8f09b4b8fb68d38bb89a1892329f9f98b9d3
Stanley Ma <stanley.ma@live.com>
2017-09-13 15:59:08 -0400

    Remove reload function out of init map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -13,9 +13,6 @@ function radiusSlider() {
   }
 }
 
-// Creates Point A and B search location bars + autocomplete.
-
-
 
 function searchBarA() {
 
@@ -135,10 +132,224 @@ function autocomplete(autocompleteC) {
   })
 }
 
+function submitButton() {
+
+  var submit = document.getElementById('submit');
+
+  submit.addEventListener('click', function() {
+
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers = [];
+
+    var center = reload(locations);
+    var categories = document.getElementById("categories");
+    var categoryValue = categories.options[categories.selectedIndex].value;
+
+    //Ajax call to rails server to get Yelp api info
+
+    $.ajax({
+      url: '/map',
+      method: 'GET',
+      data: { 'lat': center["lat"], 'lng': center["lng"], 'radius': slideVal, 'term': categoryValue},
+      dataType: 'json'
+    }).always(function(data) {
+      console.log(data);
+
+      var businessName = [];
+      var businessPic = [];
+      var businessId = [];
+
+      // Collects Yelp data and places in DOM elements
+
+      for (var i = 0; i < data.businesses.length; i++) {
+        var divYelpInfo = document.querySelector('#yelp_info');
+        var divYelpList = document.createElement('div');
+        var starContainer = document.createElement('div');
+        starContainer.className = 'star-ratings-sprite';
+        var stars = document.createElement('span');
+        stars.className = 'star-ratings-sprite-rating';
+
+        var img = document.createElement('img');
+        img.className = "border";
+        var pName = document.createElement('h4');
+        var pAddress = document.createElement('p');
+        var pPhone = document.createElement('p');
+        var pPrice = document.createElement('p');
+        var pRating = document.createElement('p');
+        var pType = document.createElement('p');
+        var pUrl = document.createElement('a');
+
+        divYelpList.className = "yelplistdiv"
+        pUrl.href = data.businesses[i].url;
+        pUrl.target = "_blank";
+        img.width = "300";
+        img.height = "300";
+
+        // Replaces blank images locations with default "no image pic"
+        if (data.businesses[i].image_url) {
+          img.src = data.businesses[i].image_url;
+        } else {
+          img.src = "http://skolarships.com/admin/scholarship_images/no-img.jpg"
+        }
+        pName.innerHTML = data.businesses[i].name.bold();
+        pName.id = data.businesses[i].id;
+        pType.innerHTML = data.businesses[i].categories[0]["title"];
+        pAddress.innerHTML = "➤ "+data.businesses[i].location.display_address;
+        pPhone.innerHTML = "☎ "+data.businesses[i].display_phone.substr(2);
+        pPrice.innerHTML = data.businesses[i].price;
+        pRating.innerHTML = data.businesses[i].rating;
+        pRating.innerText = (pRating.innerText / 5.0) * 100;
+        stars.style.width = pRating.innerText + "%";
+        starContainer.append(stars);
+        divYelpList.append(img);
+        divYelpList.append(pName);
+        divYelpList.append(pType);
+        divYelpList.append(pPhone);
+        divYelpList.append(pAddress);
+        divYelpList.append(starContainer);
+        pUrl.append(divYelpList);
+        divYelpInfo.append(pUrl);
+
+        // Stores Yelp results locations
+        var yelpLat = data.businesses[i].coordinates.latitude;
+        var yelpLong = data.businesses[i].coordinates.longitude;
+
+
+        businessName.push(data.businesses[i].name);
+        businessPic.push(data.businesses[i].image_url);
+        businessId.push(data.businesses[i].id);
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker, i;
+
+        // Places markers on map based on Yelp results
+        for (i = 0; i < businessName.length; i++) {
+
+          var myLatlng = {lat: yelpLat, lng: yelpLong};
+          var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+          });
+
+          marker.setMap(map);
+
+          // Function for adding pics and names to marker infowindow
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              var div = document.createElement('div');
+              var name = document.createElement('p');
+              var img = document.createElement('img');
+              var a = document.createElement('a');
+
+              if (businessPic[i]) {
+                img.src = businessPic[i];
+              } else {
+                img.src = "http://skolarships.com/admin/scholarship_images/no-img.jpg";
+              }
+
+              img.align = "left";
+              img.style.width = "150px";
+              img.style.height = "150px";
+              img.border = "2px";
+              name.innerHTML = businessName[i].bold();
+
+              div.appendChild(name);
+              div.append(img);
+
+
+              infowindow.setContent(div.innerHTML);
+              infowindow.open(map, marker);
+              console.log(img, name, a );
+            }
+          })(marker, i));
+        }
+      }
+    });
+
+    // Places makers on center point of all locations
+    var marker = new google.maps.Marker({
+      position: center,
+      map: map,
+    });
+
+    // Places radius around center markers. Radius value is taken from radius slider
+    var circle = new google.maps.Circle({
+      map: map,
+      radius: slideVal * 2,    // 10 miles in metres
+      fillOpacity: 0.1,
+      fillColor: '#AA0000'
+    });
+    circle.bindTo('center', marker, 'position');
+
+  })
+}
+
+function reload(locations) {
+
+//  Creates rectangular boundary using all cooridates in 'locations'
+  var bound = new google.maps.LatLngBounds();
+   for (i = 0; i < locations.length; i++) {
+   bound.extend( new google.maps.LatLng(locations[i][0], locations[i][1]) );
+   map.fitBounds(bound);
+ };
+
+// Finds center point of recanglular boundary created above and places marker. Stores center location in 'center'
+  var center = { lng: parseFloat(bound.getCenter().lng()), lat: parseFloat(bound.getCenter().lat())};
+
+  var centerMarker = new google.maps.Marker({
+    position: center,
+    map: map
+
+  });
+
+  initialize(center, centerMarker);
+  return center;
+}
+
+// Changes display parameters of map (may not need one at start?)
+  function initialize(center, centerMarker) {
+
+  // Initializes directions services
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+
+    directionsDisplay = new google.maps.DirectionsRenderer();
+
+    var awal = new google.maps.LatLng(center);
+    var mapOptions = {
+      zoom: 11,
+      center: awal
+    };
+
+    var i;
+      directionsServices = [];
+      directionsDisplays = [];
+      for (i = 0; i < locations.length; i++) {
+        directionsServices[i] = new google.maps.DirectionsService();
+        var start = new google.maps.LatLng(locations[i][0], locations[i][1]);
+
+        var request = {
+            origin: start,
+            destination: center,
+            optimizeWaypoints: true,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+
+          directionsServices[i].route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+              directionsDisplays.push(new google.maps.DirectionsRenderer({preserveViewport:true}));
+            directionsDisplays[directionsDisplays.length-1].setMap(map);
+            directionsDisplays[directionsDisplays.length-1].setDirections(response);
+          } else alert("Directions request failed:"+status);
+        });
+  }}
+
 // Initializes map
 
 function initMap() {
-
 
 // Creates map and sets starting view and zoom. Styles allows for custom map styles.
   map = new google.maps.Map(document.getElementById('map'), {
@@ -211,6 +422,8 @@ function initMap() {
 ]
   });
 
+  // Creates Point A and B search location bars + autocomplete.
+
   // Listens for autocomplete location bar A
   searchBarA();
 
@@ -222,229 +435,13 @@ function initMap() {
 
   // Allows for adding additional locations bars.
   additionalLocationBar();
-  
 
+  // Click function for submit button
+  submitButton();
 
+  // Creates a rectangular bound of all given locations, then finds center point and places a marker
+  reload(locations);
 
-
-    var submit = document.getElementById('submit');
-
-    // Click function for submit button
-
-    submit.addEventListener('click', function(){
-
-      for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-    }
-      markers = [];
-
-      var center = reload(locations);
-      var categories = document.getElementById("categories");
-      var categoryValue = categories.options[categories.selectedIndex].value;
-
-      //Ajax call to rails server to get Yelp api info
-
-      $.ajax({
-        url: '/map',
-        method: 'GET',
-        data: { 'lat': center["lat"], 'lng': center["lng"], 'radius': slideVal, 'term': categoryValue},
-        dataType: 'json'
-      }).always(function(data) {
-          console.log(data);
-
-          var businessName = [];
-          var businessPic = [];
-          var businessId = [];
-
-          // Collects Yelp data and places in DOM elements
-
-          for (var i = 0; i < data.businesses.length; i++) {
-            var divYelpInfo = document.querySelector('#yelp_info');
-            var divYelpList = document.createElement('div');
-            var starContainer = document.createElement('div');
-            starContainer.className = 'star-ratings-sprite';
-            var stars = document.createElement('span');
-            stars.className = 'star-ratings-sprite-rating';
-
-            var img = document.createElement('img');
-            img.className = "border";
-            var pName = document.createElement('h4');
-            var pAddress = document.createElement('p');
-            var pPhone = document.createElement('p');
-            var pPrice = document.createElement('p');
-            var pRating = document.createElement('p');
-            var pType = document.createElement('p');
-            var pUrl = document.createElement('a');
-
-            divYelpList.className = "yelplistdiv"
-            pUrl.href = data.businesses[i].url;
-            pUrl.target = "_blank";
-            img.width = "300";
-            img.height = "300";
-
-            // Replaces blank images locations with default "no image pic"
-            if (data.businesses[i].image_url) {
-              img.src = data.businesses[i].image_url;
-            } else {
-              img.src = "http://skolarships.com/admin/scholarship_images/no-img.jpg"
-            }
-            pName.innerHTML = data.businesses[i].name.bold();
-            pName.id = data.businesses[i].id;
-            pType.innerHTML = data.businesses[i].categories[0]["title"];
-            pAddress.innerHTML = "➤ "+data.businesses[i].location.display_address;
-            pPhone.innerHTML = "☎ "+data.businesses[i].display_phone.substr(2);
-            pPrice.innerHTML = data.businesses[i].price;
-            pRating.innerHTML = data.businesses[i].rating;
-            pRating.innerText = (pRating.innerText / 5.0) * 100;
-            stars.style.width = pRating.innerText + "%";
-            starContainer.append(stars);
-            divYelpList.append(img);
-            divYelpList.append(pName);
-            divYelpList.append(pType);
-            divYelpList.append(pPhone);
-            divYelpList.append(pAddress);
-            divYelpList.append(starContainer);
-            pUrl.append(divYelpList);
-            divYelpInfo.append(pUrl);
-
-            // Stores Yelp results locations
-            var yelpLat = data.businesses[i].coordinates.latitude;
-            var yelpLong = data.businesses[i].coordinates.longitude;
-
-
-            businessName.push(data.businesses[i].name);
-            businessPic.push(data.businesses[i].image_url);
-            businessId.push(data.businesses[i].id);
-
-            var infowindow = new google.maps.InfoWindow();
-
-            var marker, i;
-
-            // Places markers on map based on Yelp results
-            for (i = 0; i < businessName.length; i++) {
-
-              var myLatlng = {lat: yelpLat, lng: yelpLong};
-              var marker = new google.maps.Marker({
-                  position: myLatlng,
-                  map: map,
-                });
-
-              marker.setMap(map);
-
-              // Function for adding pics and names to marker infowindow
-              google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                return function() {
-                  var div = document.createElement('div');
-                  var name = document.createElement('p');
-                  var img = document.createElement('img');
-                  var a = document.createElement('a');
-
-                  if (businessPic[i]) {
-                    img.src = businessPic[i];
-                  } else {
-                    img.src = "http://skolarships.com/admin/scholarship_images/no-img.jpg";
-                  }
-
-                  img.align = "left";
-                  img.style.width = "150px";
-                  img.style.height = "150px";
-                  img.border = "2px";
-                  name.innerHTML = businessName[i].bold();
-
-                  div.appendChild(name);
-                  div.append(img);
-
-
-                  infowindow.setContent(div.innerHTML);
-                  infowindow.open(map, marker);
-                  console.log(img, name, a );
-                }
-              })(marker, i));
-            }
-        }
-    });
-
-    // Places makers on center point of all locations
-    var marker = new google.maps.Marker({
-      position: center,
-      map: map,
-    });
-
-    // Places radius around center markers. Radius value is taken from radius slider
-    var circle = new google.maps.Circle({
-      map: map,
-      radius: slideVal * 2,    // 10 miles in metres
-      fillOpacity: 0.1,
-      fillColor: '#AA0000'
-    });
-    circle.bindTo('center', marker, 'position');
-
-  })
-
-
-
-
-  function reload(locations) {
-
-  //  Creates rectangular boundary using all cooridates in 'locations'
-    var bound = new google.maps.LatLngBounds();
-     for (i = 0; i < locations.length; i++) {
-     bound.extend( new google.maps.LatLng(locations[i][0], locations[i][1]) );
-     map.fitBounds(bound);
-   };
-
-  // Finds center point of recanglular boundary created above and places marker. Stores center location in 'center'
-    var center = { lng: parseFloat(bound.getCenter().lng()), lat: parseFloat(bound.getCenter().lat())};
-
-    var centerMarker = new google.maps.Marker({
-      position: center,
-      map: map
-
-    });
-
-    initialize(center, centerMarker);
-    return center;
-}
-
-
-// Changes display parameters of map (may not need one at start?)
-  function initialize(center, centerMarker) {
-
-  // Initializes directions services
-    var directionsDisplay;
-    var directionsService = new google.maps.DirectionsService();
-
-    directionsDisplay = new google.maps.DirectionsRenderer();
-
-    var awal = new google.maps.LatLng(center);
-    var mapOptions = {
-      zoom: 11,
-      center: awal
-    };
-
-    var i;
-      directionsServices = [];
-      directionsDisplays = [];
-      for (i = 0; i < locations.length; i++) {
-        directionsServices[i] = new google.maps.DirectionsService();
-        var start = new google.maps.LatLng(locations[i][0], locations[i][1]);
-
-        var request = {
-            origin: start,
-            destination: center,
-            optimizeWaypoints: true,
-            travelMode: google.maps.TravelMode.DRIVING
-        };
-
-          directionsServices[i].route(request, function(response, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
-              directionsDisplays.push(new google.maps.DirectionsRenderer({preserveViewport:true}));
-            directionsDisplays[directionsDisplays.length-1].setMap(map);
-            directionsDisplays[directionsDisplays.length-1].setDirections(response);
-          } else alert("Directions request failed:"+status);
-        });
-  }}
-
-      google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', initialize);
 
 }
